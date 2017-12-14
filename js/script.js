e94d3c9c6416334a7392d780a940f265e36a27f0
judyzaratan <judy.zaratan@gmail.com>
2017-12-14 01:02:27 -0800

    feat: updating temperature unit

--- js/script.js
+++ js/script.js
@@ -1,6 +1,16 @@
-function convertTemp(celcius){
-  var farhenheit = (celcius * (9/5)) + 32;
-  return farhenheit;
+function convertTemp(temp){
+  var $tempUnit = $('#tempUnit');
+  var updateValue = $tempUnit.html();
+  if (updateValue === 'C') {
+    var farhenheit = (temp * (9/5)) + 32;
+    $("#temp").html(farhenheit.toPrecision(4).toString());
+    $tempUnit.html('F');
+  } else {
+    var celcius = (temp - 32) * (5/9);
+    $("#temp").html(celcius.toPrecision(4).toString());
+    $tempUnit.html('C');
+
+  }
 }
 
 function getLocation() {
@@ -12,8 +22,8 @@ function getLocation() {
 }
 function changeTempUnit() {
   var $tempUnit = $('#tempUnit');
-  $tempUnit.html('C');
-  $tempUnit.html('F');
+  var updateValue = $tempUnit.html() === 'C' ? 'F': 'C';
+  $tempUnit.html(updateValue);
 }
 function showPosition(position) {
   var $temp = $("#temp");
@@ -35,6 +45,7 @@ function showPosition(position) {
       $temp.html(response.main.temp);
       $city.html(response.name);
       $desc.html(response.weather[0].main);
+      $tempUnit.html('C');
       var pic = response.weather[0].icon || null;
       if (pic) {
         $pic.html("<img src = '" + pic + "' " + "alt='"
@@ -45,4 +56,10 @@ function showPosition(position) {
 }
 $(document).ready(function(){
   getLocation();
+  $("#tempUnit").click(function() {
+    var temp = parseInt($('#temp').html());
+    convertTemp(temp);
+
+  });
+
 });
